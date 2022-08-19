f8b0a28f8ce97994c6ae34136eb66e8e795aadc4
Олег Росляков <111491697+OlegRoslyakov@users.noreply.github.com>
2022-08-19 18:52:01 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream> //
+
+int main()  
+{
+  short data = 255;
+  std::cout << data << std::endl;
+   float payload = 6.153 ;
+  std::cout << payload << std::endl;
+   bool flag = true ;
+    std::cout << std::boolalpha <<flag<< std::endl; 
+  
+  char ch(35);
+  std::cout << ch << std::endl<< std::endl;
+
+#include <iostream> //
+
+
+
+  int a;
+  a = 10;// Не совсем понял как изменить переменную на 10
+  std::cout << a << std::endl;
+  a =a + 100;
+    std::cout << a << std::endl;
+  a =a - 20;
+    std::cout << a << std::endl;
+  a =a * 3;
+  std::cout << a << std::endl;
+  a =a / 2;
+   std::cout << a << std::endl<< std::endl;
+
+  #include <iostream> //поставили запятую 1,5426 и не вывели на экран
+   float g = 0;
+    float h = 1;;
+     double q = 1.5426;
+  std::cout << q << std::endl;
+
+  int value = 3;   //std::не прописали
+     value = value + 1;
+   std::cout << value << std::endl;
+
+  int userID = 0x12;// std::не прописали Cout и Endl с большой нельзя
+    userID += 5;
+    std::cout << userID << std::endl;
+  
+  
+  
+  
+  
+  
+  }
